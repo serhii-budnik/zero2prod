568f6b0776425647a0e56fa088d6bb486d43c229
Serhii Budnik <serhiib@scribd.com>
2023-05-07 14:27:46 +0300

    refactor run function not to be async and first test :tada:

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,22 @@
-use zero2prod::main;
+use reqwest;
 
-#[test]
-fn dummy_test() {
-    main()
+#[tokio::test]
+async fn health_check_works() {
+    spawn_app();
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://localhost:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+
+    let _ = tokio::spawn(server);
 }
