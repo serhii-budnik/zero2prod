97bceeb5d273d11d06c2fafcbad130bc69b3c565
Serhii Budnik <serhiib@scribd.com>
2023-06-07 16:26:52 +0300

    improve specs

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -19,10 +19,20 @@ impl AsRef<str> for SubscriberEmail {
 
 #[cfg(test)]
 mod tests {
-    use claim::{assert_ok, assert_err};
+    use claim::assert_err;
     use fake::Fake;
     use fake::faker::internet::en::SafeEmail;
     use super::SubscriberEmail;
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
 
     #[test]
     fn empty_string_is_rejected() {
@@ -42,9 +52,8 @@ mod tests {
         assert_err!(SubscriberEmail::parse(email));
     }
 
-    #[test]
-    fn a_valid_email_is_parsed_successfully() {
-        let email = SafeEmail().fake();
-        assert_ok!(SubscriberEmail::parse(email));
+    #[quickcheck_macros::quickcheck]
+    fn a_valid_emails_is_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
