a09f21d68b9452913756cc95dac61cb97527b471
Serhii Budnik <serhiib@scribd.com>
2023-06-23 10:17:42 +0300

    add redis storage

--- src/startup.rs
+++ src/startup.rs
@@ -1,3 +1,4 @@
+use actix_session::{SessionMiddleware, storage::RedisSessionStore};
 use actix_web::cookie::Key;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
@@ -20,26 +21,30 @@ pub struct Application {
 
 pub struct ApplicationBaseUrl(pub String);
 
-pub fn run(
+pub async fn run(
     listener: TcpListener,
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
     hmac_secret: Secret<String>,
-) -> Result<Server, std::io::Error> {
+    redis_uri: Secret<String>,
+) -> Result<Server, anyhow::Error> {
     let db_pool = web::Data::new(db_pool);
     let email_client = web::Data::new(email_client);
     let base_url = web::Data::new(ApplicationBaseUrl(base_url));
 
-    let message_store = CookieMessageStore::builder(
-        Key::from(hmac_secret.expose_secret().as_bytes()),
-    ).build();
+    let secret_key = Key::from(hmac_secret.expose_secret().as_bytes());
+
+    let message_store = CookieMessageStore::builder(secret_key.clone()).build();
     let message_framework = FlashMessagesFramework::builder(message_store).build();
+
+    let redis_store = RedisSessionStore::new(redis_uri.expose_secret()).await?;
 
     let server = HttpServer::new(move || { 
         App::new()
-            .wrap(TracingLogger::default())
             .wrap(message_framework.clone())
+            .wrap(SessionMiddleware::new(redis_store.clone(), secret_key.clone()))
+            .wrap(TracingLogger::default())
             .route("/", web::get().to(home))
             .route("/health_check", web::get().to(health_check))
             .route("/login", web::get().to(login_form))
@@ -58,7 +63,7 @@ pub fn run(
 }
 
 impl Application {
-    pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
+    pub async fn build(configuration: Settings) -> Result<Self, anyhow::Error> {
         let connection_pool = get_connection_pool(&configuration.database);
 
         let sender_email = configuration
@@ -84,8 +89,9 @@ impl Application {
             connection_pool,
             email_client,
             configuration.application.base_url,
-            hmac_secret
-        )?;
+            hmac_secret,
+            configuration.redis_uri,
+        ).await?;
 
         Ok(Self { port, server })
     }
