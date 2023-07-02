6aa99fedead251ffca2185912fe4ee531731f522
Serhii <serhii.pab@gmail.com>
2023-07-02 16:58:26 +0300

    add stub for user_password struct

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 current page: 379
 
-10.8.2.3
+10.8.2.4
+
+TODO: add validation for password:
+* min 12 
+* max 128
---
+++ src/domain/user_password.rs
@@ -0,0 +1,7 @@
+pub struct UserPassword(String);
+
+impl UserPassword { 
+    fn validate_password(&self) -> bool {
+        todo!()
+    }
+}
