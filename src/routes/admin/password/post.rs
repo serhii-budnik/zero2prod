d7bc660256e1f09e305d05eaf0df977f9f8c511e
Serhii <serhii.pab@gmail.com>
2023-07-02 16:11:58 +0300

    handle incorrect passwords on reset page

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,7 +1,9 @@
 use crate::routes::helpers::ApiError;
+use crate::utils::see_other;
 
 use actix_web::{HttpResponse, web};
-use secrecy::Secret;
+use actix_web_flash_messages::FlashMessage;
+use secrecy::{Secret, ExposeSecret};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -13,5 +15,11 @@ pub struct FormData {
 pub async fn change_password(
     form: web::Form<FormData>,
 ) -> Result<HttpResponse, ApiError> {
+    if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
+        FlashMessage::error("You entered two different new passwords - the field values must match.").send();
+
+        return Ok(see_other("/admin/password"));
+    }
+
     todo!()
 }
--- tests/api/change_password.rs
+++ tests/api/change_password.rs
@@ -26,4 +26,30 @@ async fn you_must_be_logged_in_to_change_your_password() {
     assert_is_redirect_to(&response, "/login");
 }
 
+#[tokio::test]
+async fn new_password_fields_must_match() {
+    let app = spawn_app().await;
+    let new_password = Uuid::new_v4().to_string();
+    let another_new_password = Uuid::new_v4().to_string();
 
+    let (username, password) = app.add_test_user().await;
+
+
+    app.post_login(&serde_json::json!({
+        "username": username,
+        "password": &password,
+    })).await;
+
+    let response = app.post_change_password(&serde_json::json!({
+        "current_password": &password,
+        "new_password": &new_password,
+        "new_password_check": &another_new_password,
+    })).await;
+
+    assert_is_redirect_to(&response, "/admin/password");
+
+    let html_page = app.get_change_password_html().await;
+    assert!(html_page.contains(
+        "<p><i>You entered two different new passwords - the field values must match.</i></p>"
+    ))
+}
